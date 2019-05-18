4cbaecb16bfc0f99b98c13ef40aa2662ceccea9e
Martin Roldan <marttinroldan@gmail.com>
2019-05-18 03:16:03 -0400

    Added timeout

--- src/Geocoder.php
+++ src/Geocoder.php
@@ -2,11 +2,13 @@
 
 namespace Mroldan\Geocoder;
 
-class Geocoder {
+class Geocoder
+{
 
     private $cache = [];
-    private $proxyList = null;//set in downloadProxyList 
+    private $proxyList = null; //set in downloadProxyList 
     private $currentProxy = null;
+    public $timeout = 1; //seconds
 
     public $useProxy = false;
     public $debug = false;
@@ -14,7 +16,7 @@ class Geocoder {
 
     public $sourceURL = "http://geocoder.ca";
     public $options = [
-        'json'=>1,
+        'json' => 1,
     ]; //can be customized
 
     public $errorTypes = [
@@ -23,25 +25,28 @@ class Geocoder {
         '400 Bad Request',
         'Authentication Required',
     ];
-    
 
-    public function serviceURL($location){
+
+    public function serviceURL($location)
+    {
         $this->options['locate'] = $location;
         $queryParams = \http_build_query($this->options);
-        return sprintf("%s?%s",$this->sourceURL,$queryParams);
+        return sprintf("%s?%s", $this->sourceURL, $queryParams);
     }
 
-    public function cacheGet($key){
+    public function cacheGet($key)
+    {
         $value = null;
 
-        if(array_key_exists($key, $this->cache)){
+        if (array_key_exists($key, $this->cache)) {
             $value = $this->cache[$key];
         }
 
         return $value;
     }
 
-    public function cacheSet($key,$value){
+    public function cacheSet($key, $value)
+    {
         $this->cache[$key] = $value;
         return $this->cacheGet($key);
     }
@@ -51,38 +56,40 @@ class Geocoder {
      * @param string $location
      * @return Object|null
      */
-    public function locate($location){
+    public function locate($location)
+    {
         $results = null;
         $contents = $this->cacheGet($location);
-        if(!$contents){
+        if (!$contents) {
             $url = $this->serviceURL($location);
 
-            try{
+            try {
                 $contents = $this->fileGetContents($url);
-                if($contents){
+                if ($contents) {
                     $this->log($contents);
-                    $this->cacheSet($location,$contents);
+                    $this->cacheSet($location, $contents);
                     $results = json_decode($contents);
                 }
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 $results = null;
             }
-        }else{
+        } else {
             $results = json_decode($contents);
         }
-    
+
         return $results;
     }
 
-    public function downloadProxyList(){
+    public function downloadProxyList()
+    {
         $proxyList = [];
         $url = $this->proxyListURL;
 
         $contents = file_get_contents($url);
 
-        if($contents){
-            $proxyList = explode("\n",$contents);
-            $proxyList = array_filter($proxyList, function($item){
+        if ($contents) {
+            $proxyList = explode("\n", $contents);
+            $proxyList = array_filter($proxyList, function ($item) {
                 return !empty($item);
             });
         }
@@ -90,25 +97,30 @@ class Geocoder {
         return $proxyList;
     }
 
-    public function nextProxy(){
-        if(is_null($this->proxyList)){
+    public function nextProxy()
+    {
+        if (is_null($this->proxyList)) {
             $this->proxyList = $this->downloadProxyList();
         }
         $this->currentProxy = array_shift($this->proxyList);
     }
 
 
-    public function fileGetContents($url){
-        
+    public function fileGetContents($url)
+    {
+
         $ch = curl_init();
         // set url
+
+        $timeStart = microtime(true);
         curl_setopt($ch, CURLOPT_URL, $url);
 
         //return the transfer as a string
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1000);
 
-        if($this->currentProxy){
+        if ($this->currentProxy) {
             $this->log("using proxy $this->currentProxy");
             curl_setopt($ch, CURLOPT_PROXY, $this->currentProxy);
         }
@@ -117,19 +129,22 @@ class Geocoder {
         $contents = curl_exec($ch);
 
         // close curl resource to free up system resources
-        curl_close($ch); 
+        curl_close($ch);
+        $timeEnd = microtime(true);
+        $lasted = $timeEnd - $timeStart;
 
         $error = $this->parseErrors($contents);
 
-        if($error && $this->useProxy){
+        $this->log("Took $lasted");
+        if (!$contents || (($error && $lasted <= $this->timeout) && $this->useProxy)) {
 
             $this->log($error);
             $this->nextProxy();
-            
-            if($this->currentProxy){
+
+            if ($this->currentProxy) {
                 $this->log("Trying with proxy... $this->currentProxy");
                 $contents = $this->fileGetContents($url);
-            }else{
+            } else {
                 $this->log('No more proxies to try');
                 $contents = null;
             }
@@ -138,9 +153,10 @@ class Geocoder {
         return $contents;
     }
 
-    private function parseErrors($string){
-        foreach($this->errorTypes as $errorType){
-            if(strpos($string,$errorType) !== false){
+    private function parseErrors($string)
+    {
+        foreach ($this->errorTypes as $errorType) {
+            if (strpos($string, $errorType) !== false) {
                 return $errorType;
             }
         }
@@ -148,8 +164,9 @@ class Geocoder {
         return false;
     }
 
-    private function log($msg){
-        if($this->debug){
+    private function log($msg)
+    {
+        if ($this->debug) {
             error_log($msg);
         }
     }
